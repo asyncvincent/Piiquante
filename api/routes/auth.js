d5b9150d7b3ba7bfa9c01fbfce2637e4b87e074e
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-08-30 15:30:52 +0200

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const authController = require('../controllers/auth.controller');
 const validation = require('../middleware/validation');
+const authController = require('../controllers/auth.controller');
 
 router.post('/signup', validation.signupValidation, authController.signup);
+router.post('/login', validation.loginValidation, authController.login);
 
 module.exports = router;
