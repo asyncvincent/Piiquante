e271d8c1ac38ef7ab5e42812713632a38a48d35a
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-08-30 15:30:31 +0200

    Update validation.js

--- api/middleware/validation.js
+++ api/middleware/validation.js
@@ -2,10 +2,13 @@ const {
     body,
     validationResult
 } = require('express-validator');
-const checkValidationErrors = (req, res, next) => {
+
+const validate = require('mongoose-validator');
+
+function checkValidationErrors(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({
+        return res.status(403).json({
             message: errors.array()[0].msg
         });
     }
@@ -13,20 +16,30 @@ const checkValidationErrors = (req, res, next) => {
 }
 
 exports.signupValidation = [
-    body('email').isEmail().withMessage(`Email must be a valid email`),
+    body('email').isEmail().withMessage(`L'adresse email n'est pas valide`),
     body('password').isLength({
-        min: 5
-    }).withMessage(`Password must be at least 5 characters long`),
+        min: 6
+    }).withMessage(`Password must be at least 6 characters long`),
     // body('password')
     //     .isStrongPassword({
     //         minLength: 8,
-    //         // minLowercase: 1,
-    //         // minUppercase: 1,
-    //         // minNumbers: 1,
-    //         // minSymbols: 1
+    //         minLowercase: 1,
+    //         minUppercase: 1,
+    //         minNumbers: 1,
+    //         minSymbols: 1
     //     })
     //     .withMessage(
     //         `Password must be at least 8 characters long, contain at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 symbol`),
+    (req, res, next) => {
+        checkValidationErrors(req, res, next);
+    }
+];
+
+exports.loginValidation = [
+    body('email').isEmail().withMessage(`L'adresse email n'est pas valide`),
+    body('password').isLength({
+        min: 6
+    }).withMessage(`Le mot de passe doit contenir au moins 6 caractères`),
     (req, res, next) => {
         checkValidationErrors(req, res, next);
     }
