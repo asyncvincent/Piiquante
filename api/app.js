978dbef917dfc87257d01059f0b1a03eaef40a98
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-08-30 15:29:29 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+const session = require('cookie-session');
+const helmet = require('helmet');
 require('dotenv').config();
-const Router = express.Router();
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -14,8 +16,23 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log('Error:', err.message);
 });
 
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(session({
+    name: 'session',
+    keys: [process.env.SESSION_KEY],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        domain: 'localhost:3000'
+    }
+}));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, UPDATE');
@@ -28,7 +45,8 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/sauces', require('./routes/sauce'));
 
 module.exports = app;
