6f8cd732478b95f18c1907e2473f5dc4ba1cea4f
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-09-04 17:40:43 +0200

    Update validation.js

--- api/middleware/validation.js
+++ api/middleware/validation.js
@@ -3,44 +3,59 @@ const {
     validationResult
 } = require('express-validator');
 
-const validate = require('mongoose-validator');
-
+// Check validation errors
 function checkValidationErrors(req, res, next) {
+
+    // Get validation errors
     const errors = validationResult(req);
+
+    // Check if there are errors
     if (!errors.isEmpty()) {
+
+        // Return errors
         return res.status(403).json({
             message: errors.array()[0].msg
         });
     }
+
+    // Call next middleware
     next();
 }
 
-exports.signupValidation = [
-    body('email').isEmail().withMessage(`L'adresse email n'est pas valide`),
-    body('password').isLength({
-        min: 6
-    }).withMessage(`Password must be at least 6 characters long`),
-    // body('password')
-    //     .isStrongPassword({
-    //         minLength: 8,
-    //         minLowercase: 1,
-    //         minUppercase: 1,
-    //         minNumbers: 1,
-    //         minSymbols: 1
-    //     })
-    //     .withMessage(
-    //         `Password must be at least 8 characters long, contain at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 symbol`),
-    (req, res, next) => {
-        checkValidationErrors(req, res, next);
-    }
-];
+module.exports = {
 
-exports.loginValidation = [
-    body('email').isEmail().withMessage(`L'adresse email n'est pas valide`),
-    body('password').isLength({
-        min: 6
-    }).withMessage(`Le mot de passe doit contenir au moins 6 caractères`),
-    (req, res, next) => {
-        checkValidationErrors(req, res, next);
-    }
-];
+    // Signup validation Middleware
+    SignupValidation: [
+
+        // Check email
+        body('email').isEmail().withMessage({
+            message: "L'adresse email n'est pas valide"
+        }),
+
+        // Check password
+        body('password').isLength({
+            min: 8
+        }).withMessage({
+            message: "Le mot de passe doit contenir au moins 8 caractères"
+        }),
+
+        // Check validation errors
+        (req, res, next) => {
+            checkValidationErrors(req, res, next);
+        }
+    ],
+
+    // Login Validation Middleware
+    LoginValidation: [
+
+        // Check email
+        body('email').isEmail().withMessage({
+            message: "L'adresse email n'est pas valide"
+        }),
+
+        // Check validation errors
+        (req, res, next) => {
+            checkValidationErrors(req, res, next);
+        }
+    ]
+};
