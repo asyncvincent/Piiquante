a4b7a56f7d1c25735a089270b7d840151980193b
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-09-04 17:40:40 +0200

    Update authentication.js

--- api/middleware/authentication.js
+++ api/middleware/authentication.js
@@ -1,22 +1,34 @@
 const jwt = require('jsonwebtoken');
-
 const SESSION_KEY = process.env.SESSION_KEY;
 
-module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, SESSION_KEY);
-        const userId = decodedToken.userId;
+module.exports = {
 
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'Invalid user ID';
-        } else {
-            next();
+    // Authentication middleware
+    Authentication: (req, res, next) => {
+
+        try {
+
+            // Get token from headers
+            const token = req.headers.authorization.split(' ')[1];
+
+            // Decode token with session key
+            const decodedToken = jwt.verify(token, SESSION_KEY);
+
+            // Get user id from decoded token 
+            const userId = decodedToken.userId;
+
+            // Check if user id exists
+            if (req.body.userId && req.body.userId !== userId) {
+                // Return error
+                throw "L'identifiant de l'utilisateur n'est pas valide";
+            } else {
+                // Call next middleware
+                next();
+            }
+        } catch (err) { // Catch errors
+            return res.status(401).json({
+                message: "Authentification échouée"
+            });
         }
-    } catch (err) {
-        console.log(err);
-        res.status(401).json({
-            error: new Error('Invalid request!')
-        });
     }
 }
