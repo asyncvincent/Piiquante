21078bf584981f07eb8b744b5d780a6af41f306c
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-09-04 17:40:36 +0200

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,70 +1,81 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-
 const SESSION_KEY = process.env.SESSION_KEY;
 
 module.exports = {
-    signup: async (req, res) => {
+
+    // Create new user account 
+    Signup: async (req, res) => {
+
+        // Get email and password from request body
         const { email, password } = req.body;
-        try {
-            const user = await User.findOne({ email });
-            if (user) {
-                return res.status(400).json({
-                    message: 'User already exists'
-                });
-            }
-            const hashedPassword = await bcrypt.hash(password, 12);
-            const newUser = new User({
-                email,
-                password: hashedPassword
-            });
-            await newUser.save();
-            const token = jwt.sign({ userId: newUser._id }, SESSION_KEY, { expiresIn: '1h' });
-            console.log(token);
-            res.status(201).json({
-                message: 'User created successfully',
+
+        // Hash password with bcrypt
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        // Create new user 
+        const user = new User({
+            email,
+            password: hashedPassword
+        });
+
+        // Create token with jwt
+        const token = jwt.sign({ userId: user._id }, SESSION_KEY, { expiresIn: '24h' });
+
+        // Save user in database
+        await user.save()
+            .then(() => res.status(201).json({
+                message: "L'utilisateur a été créé avec succès",
                 token
-            });
-        } catch (err) {
-            res.status(500).json({
-                message: 'Something went wrong'
-            });
-        }
+            }))
+            .catch(error => res.status(400).json({
+                error: "L'utilisateur n'a pas pu être créé",
+                message: error.message
+            }));
+
     },
 
-    login: async (req, res) => {
+    // Login user account
+    Login: async (req, res) => {
+
+        // Get email and password from request body
         const { email, password } = req.body;
-        try {
 
-            const user = await User.findOne({ email });
-            if (!user) {
-                return res.status(400).json({
-                    message: 'User does not exist'
-                });
-            }
+        // Find user in database
+        const user = await User.findOne({ email });
 
-            const isMatch = await bcrypt.compare(password, user.password);
-            if (!isMatch) {
-                return res.status(400).json({
-                    message: 'Invalid credentials'
-                });
-            }
-
-            const token = jwt.sign({ userId: user._id }, SESSION_KEY, { expiresIn: '24h' });
-            res.status(200).json({
-                userId: user._id,
-                token
-            });
-
-        } catch (err) {
-            res.status(500).json({
-                message: 'Something went wrong'
+        // If user doesn't exist
+        if (!user) {
+            return res.status(401).json({
+                error: "L'utilisateur n'existe pas",
+                errorType: "userNotFound"
             });
         }
+
+        // Compare password with hashed password in database with bcrypt
+        const pwdMatch = await bcrypt.compare(password, user.password);
+
+        // Create token with jwt
+        const token = jwt.sign({ userId: user._id }, SESSION_KEY, { expiresIn: '24h' });
+
+        req.session.token = {
+            token
+        }
+
+        // Check if email and password match
+        if (!pwdMatch) {
+            return res.status(401).json({
+                error: "Le mot de passe est incorrect",
+                errorType: "PasswordIncorrect"
+            });
+        }
+
+        // Return user data
+        res.status(200).json({
+            message: "L'utilisateur a été connecté avec succès",
+            userId: user._id,
+            token
+        });
     }
 }
-
-
-
-
