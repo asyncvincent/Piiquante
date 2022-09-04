1e3658f84b545b180c5a6f0054db039dc99a1a1f
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-09-04 17:53:38 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,20 +2,26 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+// Set express to use this port and listen for requests
 app.set('port', port);
 
+// Create HTTP server
 const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces.
 server.listen(port);
+
+// Event listener for HTTP server "error" event.
 server.on('error', onError);
+
+// Event listener for HTTP server "listening" event.
 server.on('listening', onListening);
 
 function onError(error) {
     if (error.syscall !== 'listen') {
         throw error;
     }
-
     const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges');
